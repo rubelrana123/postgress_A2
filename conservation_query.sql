43b1d03b4cba94a5234a389e51f3cea9627c637b
rubelrana123 <rubelranatpi707458@gmail.com>
2025-05-26 23:23:22 +0600

    final quey check ok

--- conservation_query.sql
+++ conservation_query.sql
@@ -2,68 +2,50 @@ CREATE DATABASE conservation_db;
 
 CREATE Table rangers (
     ranger_id SERIAL PRIMARY KEY,
-    name VARCHAR(50),
-    region VARCHAR(100)
-);
+    name VARCHAR(50) NOT NULL,
+    region VARCHAR(100) NOT NULL
+)
 
-INSERT INTO
-    rangers (name, region)
-VALUES (
-       
-        'Alice Green',
-        'Northern Hills'
-    ),
-    ('Bob White', 'River Delta'),
-    (
-      
-        'Carol King',
-        'Mountain Range'
-    ),
-        (
-      
-        'Rubel Rana',
-        'Road Area'
-    );
 
-CREATE table species (
+INSERT INTO rangers (name, region)
+VALUES ('Alice Green','Northern Hills'),
+       ('Bob White', 'River Delta'),
+       ( 'Carol King','Mountain Range');
+    
+CREATE Table species (
     species_id SERIAL PRIMARY KEY,
-    common_name VARCHAR(50),
-    scientific_name VARCHAR(50),
-    discovery_date DATE,
-    conservation_status VARCHAR(50)
-);
+    common_name VARCHAR(100) NOT NULL,
+    scientific_name VARCHAR(50) NOT NULL UNIQUE,
+    discovery_date DATE NOT NULL,
+    conservation_status TEXT
+)
 
 INSERT INTO
     species (
-        species_id,
         common_name,
         scientific_name,
         discovery_date,
         conservation_status
     )
 VALUES (
-        1,
         'Snow Leopard',
         'Panthera uncia',
         '1775-01-01',
         'Endangered'
     ),
     (
-        2,
         'Bengal Tiger',
         'Panthera tigris tigris',
         '1758-01-01',
         'Endangered'
     ),
     (
-        3,
         'Red Panda',
         'Ailurus fulgens',
         '1825-01-01',
         'Vulnerable'
     ),
     (
-        4,
         'Asiatic Elephant',
         'Elephas maximus indicus',
         '1758-01-01',
@@ -72,16 +54,15 @@ VALUES (
 
 CREATE TABLE sightings (
     sighting_id SERIAL PRIMARY KEY,
-    ranger_id INT REFERENCES rangers (ranger_id) ON DELETE CASCADE,
-    species_id INT REFERENCES species (species_id) ON DELETE CASCADE,
-    sighting_time TIMESTAMP,
-    location VARCHAR(50),
+    ranger_id INTEGER NOT NULL REFERENCES rangers (ranger_id) ON DELETE CASCADE,
+    species_id INTEGER NOT NULL REFERENCES species (species_id) ON DELETE CASCADE,
+    sighting_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    location TEXT NOT NULL,
     notes TEXT
 );
 
 INSERT INTO
     sightings (
-        sighting_id,
         species_id,
         ranger_id,
         location,
@@ -91,13 +72,11 @@ INSERT INTO
 VALUES (
         1,
         1,
-        1,
         'Peak Ridge',
         '2024-05-10 07:45:00',
         'Camera trap image captured'
     ),
     (
-        2,
         2,
         2,
         'Bankwood Area',
@@ -107,13 +86,11 @@ VALUES (
     (
         3,
         3,
-        3,
         'Bamboo Grove East',
         '2024-05-15 09:10:00',
         'Feeding observed'
     ),
     (
-        4,
         1,
         2,
         'Snowfall Pass',
@@ -127,21 +104,23 @@ INSERT INTO
 VALUES ('Derek Fox', 'Coastal Plains');
 
 --2️⃣ Count unique species ever sighted. ok
-SELECT count(*) as unique_species_count
-FROM species
-WHERE
-    conservation_status = 'Endangered';
-
+SELECT count(DISTINCT species_id) as unique_species_count
+FROM sightings;
 
 --3️⃣ Find all sightings where the location includes "Pass". ok
-SELECT * FROM sightings WHERE location LIKE '%Pass';
+SELECT * FROM sightings WHERE location LIKE '%Pass%';
 
---5️⃣ List species that have never been sighted. 
---| common_name      
--- | Asiatic Elephant |
+--4️⃣ List each ranger's name and their total number of sightings.ok
+SELECT name , count(ranger_id) as total_sightings  FROM rangers
+JOIN sightings USING(ranger_id)
+GROUP BY (ranger_id,name) 
+ORDER BY name;
+
+--5️⃣ List species that have never been sighted. ok
 select common_name   FROM species
 LEFT JOIN sightings USING(species_id)
 WHERE sightings.species_id IS NULL;
+
 --6️⃣ Show the most recent 2 sightings. ok
 SELECT common_name,sighting_time,name FROM sightings
 JOIN species USING(species_id)
@@ -149,11 +128,7 @@ JOIN rangers USING (ranger_id)
 ORDER BY(sighting_time) DESC
 LIMIT 2;
 
---7️⃣ Update all species discovered before year 1800 to have status 'Historic'.
--- AffectedRows : 3
---(No output needed - this is an UPDATE operation)
-ALTER TABLE species ADD status VARCHAR(50);
---? Problem 7
+--7️⃣ Update all species discovered before year 1800 to have status 'Historic'.ok
 UPDATE species
 SET
     conservation_status = 'Historic'
@@ -163,25 +138,19 @@ WHERE
         FROM discovery_date
     ) < 1800;
 
-    SELECT * from species;
-
-
-
-
---8️⃣ Label each sighting's time of day as 'Morning', 'Afternoon', or 'Evening'.
+--8️⃣ Label each sighting's time of day as 'Morning', 'Afternoon', or 'Evening'. ok
 SELECT sighting_id,
     CASE
         WHEN EXTRACT(HOUR FROM sighting_time) < 12 THEN 'Morning'
         WHEN EXTRACT(HOUR FROM sighting_time) BETWEEN 12 AND 17 THEN 'Afternoon'
         ELSE 'Evening'
     END AS time_of_day
-FROM sightings
-ORDER BY sighting_id;
--- 9️⃣ Delete rangers who have never sighted any species
+FROM sightings;
+
+-- 9️⃣ Delete rangers who have never sighted any species ok
 DELETE FROM rangers WHERE rangers.ranger_id IN (
  SELECT rangers.ranger_id FROM rangers
  LEFT JOIN sightings 
  ON rangers.ranger_id = sightings.ranger_id
  WHERE  sightings.ranger_id IS NULL
 ); 
- SELECT * FROM rangers;
